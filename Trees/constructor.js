94116c622468c7ae01022b55663e6b6495f1b9ca
jpared3s <jparedes.wall@gmail.com>
2023-04-25 18:10:58 -0400

    tree constructor exercise

---
+++ Trees/constructor.js
@@ -0,0 +1,37 @@
+// WRITE NODE CLASS HERE //
+class Node {
+  constructor(value) {
+    this.value = value
+    this.left = null
+    this.right = null
+  }
+}
+ 
+class BST {
+	// WRITE BST CONSTRUCTOR HERE //
+constructor() {
+  this.root = null
+}
+}
+ 
+ 
+function test() {
+    let myBST = new BST();   
+    
+    if (myBST.root === null) {
+        console.log("Root: null");
+    } else {
+        console.log("Root:", myBST.root.value);
+    }
+}
+
+
+test();
+
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Root: null
+
+*/
