e941c39fc2501d7ecdd814b3fc83ca8fba090b99
jpared3s <jparedes.wall@gmail.com>
2023-04-27 10:45:03 -0400

    hashtable set exercise

--- HashTables/constructor.js
+++ HashTables/constructor.js
@@ -1,39 +1,32 @@
 class HashTable {
-	// CREATE HASHTABLE CONSTRUCTOR HERE//
-	//                                  //
-	//                                  //
-	//                                  //
-	//                                  //
-	//////////////////////////////////////
-   
-    _hash(key) {
-        let hash = 0;
-        for (let i = 0; i < key.length; i++) {
-            hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
-        }
-        return hash;
-    }
+  // CREATE HASHTABLE CONSTRUCTOR HERE//
+  constructor(size = 7) {
+    this.dataMap = new Array(size);
+  }
 
-    printTable() {
-        for (let i = 0; i < this.dataMap.length; i++) {
-            console.log(i, ": ", this.dataMap[i]);
-        }
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
     }
+    return hash;
+  }
 
+  printTable() {
+    for (let i = 0; i < this.dataMap.length; i++) {
+      console.log(i, ": ", this.dataMap[i]);
+    }
+  }
 }
-
-
 
 function test() {
-    let myHashTable = new HashTable();
-    myHashTable.printTable();  
+  let myHashTable = new HashTable();
+  myHashTable.printTable();
 }
-
 
 test();
 
-    
-    /*
+/*
         EXPECTED OUTPUT:
         ----------------
         0 :  undefined
