78c30c3cf8af8955b14002a3f3a26d7d3c947166
jpared3s <jparedes.wall@gmail.com>
2023-05-02 10:46:09 -0400

    merge sort exercise

---
+++ Sort/mergeSort.js
@@ -0,0 +1,56 @@
+function merge(array1, array2) {
+  let combined = [];
+  let i = 0;
+  let j = 0;
+  while (i < array1.length && j < array2.length) {
+      if (array1[i] < array2[j]) {
+          combined.push(array1[i]);
+          i++;
+      } else {
+          combined.push(array2[j]);
+          j++;
+      }
+  }
+  while (i < array1.length) {
+      combined.push(array1[i]);
+      i++;
+  }
+  while (j < array2.length) {
+      combined.push(array2[j]);
+      j++;
+  }
+  return combined;
+}
+
+// WRITE MERGESORT FUNCTION HERE //
+function mergeSort(array) {
+  if (array.length === 1) return array
+  let mid = Math.floor(array.length/2)
+  let left = array.slice(0,mid)
+  let right = array.slice(mid)
+  return merge(mergeSort(left), mergeSort(right))
+}
+
+
+
+
+function test() {
+  let originalArray = [3,1,4,2];
+  let sortedArray = mergeSort( originalArray );
+
+  console.log( "Original Array:", originalArray );
+  console.log( "\nSorted Array:", sortedArray );
+}
+
+
+test();
+
+
+/*
+  EXPECTED OUTPUT:
+  ----------------
+  Original Array: [ 3, 1, 4, 2 ]
+
+  Sorted Array: [ 1, 2, 3, 4 ]
+
+*/  
